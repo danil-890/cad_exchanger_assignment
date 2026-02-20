4773833a3ab1402c66a3f6ec100711558945b331
danil-890 <tutov.danil1234@gmail.com>
2026-02-20 16:42:00 +0300

    Curve.h

---
+++ curves/include/curves/Curve.h
@@ -0,0 +1,20 @@
+#ifndef CAD_EXCHANGER_CURVE_H
+#define CAD_EXCHANGER_CURVE_H
+
+#include "Point3D.h"
+
+namespace curves {
+    class Curve {
+    public:
+        virtual ~Curve() = default;
+
+        [[nodiscard]]
+        virtual Point3D getPoint(double t) const = 0;
+
+        [[nodiscard]]
+        virtual Vector3D getDerivative(double t) const = 0;
+    };
+}
+
+
+#endif //CAD_EXCHANGER_CURVE_H
---
+++ curves/include/curves/Point3D.h
@@ -0,0 +1,18 @@
+#ifndef CAD_EXCHANGER_POINT3D_H
+#define CAD_EXCHANGER_POINT3D_H
+
+namespace curves {
+    struct Point3D {
+        double x{};
+        double y{};
+        double z{};
+    };
+
+    struct Vector3D {
+        double x{};
+        double y{};
+        double z{};
+    };
+}
+
+#endif //CAD_EXCHANGER_POINT3D_H
